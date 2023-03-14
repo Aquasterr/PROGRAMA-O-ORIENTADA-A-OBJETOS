084a1a506a0f9cea5b316867ba69ba4cc999125f
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2023-03-14 11:28:03 -0300

    Update Curso.java

--- Exercicio4 - ex5 - TesteEscola/Curso.java
+++ Exercicio4 - ex5 - TesteEscola/Curso.java
@@ -4,4 +4,11 @@ public class Curso {
     private int cod_curso;
     private String desc_curso;
     private int duracao_semestre;
+    
+    public Curso (int cod_curso, String desc_curso, int duracao_semestre)
+        {
+            this.cod_curso = cod_curso;
+            this.desc_curso = desc_curso;
+        }
 }
+
